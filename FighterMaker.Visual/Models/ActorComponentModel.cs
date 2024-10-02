7b06725e45254ed9112d4e3be7414e352333a730
Danilo <borges_santos89@hotmail.com>
2024-10-02 10:59:12 -0300

    Implementa ActorModel e ActorComponentModel

---
+++ FighterMaker.Visual/Models/ActorComponentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterMaker.Visual.Models
+{
+    public class ActorComponentModel
+    {
+        public ActorModel? Actor { get; set; }
+    }
+}
